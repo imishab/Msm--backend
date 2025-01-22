53105b1b9939bbe49cf384ce2350452948bcd5cd
Mishab <85479103+imishab@users.noreply.github.com>
2025-01-22 18:17:50 +0530

    endpoint added

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ const allowedOrigins = [
     'http://localhost:3001',
     'http://localhost:3002',
     'http://192.168.1.5:3002',
-    '192.168.1.5:3002'
+    'https://zonehead.vercel.app'
 
 
 ];
